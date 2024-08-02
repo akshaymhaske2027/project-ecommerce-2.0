122fb65f368180c14fe014aca3a2963638bb831e
AKSHAY MHASKE <akshaymhaske2027@gmail.com>
2024-08-02 14:51:07 +0530

    project-structure

---
+++ src/main/java/com/akshay/project_ecommerce/ProjectEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.project_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEcommerceApplication.class, args);
+	}
+
+}
